03a5eb51188b8ef24503dbb89c39fac356c4cd72
Leo Swedérus <leo.swederus@appliedtechnology.se>
2024-10-04 14:52:20 +0200

    make-post-chat-message-to-server
    Co-authored-by: Anton <antonberglund_92@hotmail.com>
    Co-authored-by: Carl <carl@wbsweden.com>

--- frontend/src/components/chat/chat.tsx
+++ frontend/src/components/chat/chat.tsx
@@ -1,42 +1,49 @@
-import { Message } from "./message";
 
+Carl Bieneck
+  2:51 PM
+import { Message } from "./message";
+import { SendButton } from "./button";
 async function getCurrentUser() {
   return { id: 1, name: "Obi-Wan Kenobi" };
 }
-
-async function getMessages() {
-  return [
-    {
-      id: 1,
-      author: { id: 1, name: "Obi-Wan Kenobi" },
-      message: "It's over Anakin, I have the high ground.",
-    },
-    {
-      id: 2,
-      author: { id: 2, name: "Anakin Skywalker" },
-      message: "You underestimate my power!",
-    },
-  ];
+let messageData: any[] = [];
+async function getMessages(formData: FormData) {
+  'use server'
+  const response = await fetch("http://localhost:3001/messages");
+  messageData = await response.json();
+  console.log(messageData)
+  return messageData;
 }
-
+async function sendMessage(formData: FormData) {
+  'use server';
+  const message = {
+    message: {
+      name:formData.get('message')
+    }
+  }
+  const response = await fetch("http://localhost:3001/messages", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message)
+  })
+}
 export async function Chat() {
-  const messages = await getMessages();
-  const currentUser = await getCurrentUser();
-
   return (
     <article>
       <section className="py-4">
-        {messages.map((message) => (
+        {/* {messages.map((message) => (
           <Message
             key={message.id}
             currentUserId={currentUser.id}
             author={message.author}
             message={message.message}
           />
-        ))}
+        ))} */}
       </section>
-
-      <form className="form-control flex flex-row gap-2 w-full">
+      <form action={sendMessage} className="form-control flex flex-row gap-2 w-full">
         <input
           className="input input-bordered w-full"
           type="text"
@@ -44,8 +51,7 @@ export async function Chat() {
           id="message"
           name="message"
         />
-        <button className="btn btn-primary">Send</button>
-        <button className="btn btn-primary">Fetch</button>
+        <button type="submit" className="btn btn-primary">Fetch</button>
       </form>
     </article>
   );

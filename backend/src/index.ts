23a94b5c092638853e48abcc6e6cf0e7db59eb97
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-04 13:02:42 +0200

    adding-message-todb-from-req-body
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,17 +24,24 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/messages', (req: Request, res:Response) => {
- //res.json(messages);
+  const messages = JSON.parse(fs.readFileSync(messagesFilePath, 'utf8'));
+
+  res.status(200).json(messages);
   // Skicka tillbaka alla message som vi har lagrat; vart? db/fil,
   //
 })
 
 app.post('/messages', (req: Request, res:Response) => {
-  const testPerson =  {
-    "name": "Karl-Leo"
-  };
+  const message = req.body.message;
   
-  fs.appendFileSync(messagesFilePath  , JSON.stringify(testPerson));
+  const testPerson =  {
+    "name": "Leo-Karl"
+  };
+  const messagesFromDb = JSON.parse(fs.readFileSync(messagesFilePath, { encoding: 'utf-8' }));
+
+  messagesFromDb.push(message);
+  
+  fs.writeFileSync(messagesFilePath, JSON.stringify(messagesFromDb));
   // Req.body från frontend, värdet från input field, skickas när man klickar submit. 
   // Lagra det meddelandet i våran db/fil
   res.status(201).json(testPerson);
